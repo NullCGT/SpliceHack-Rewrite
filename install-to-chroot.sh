16cbe09fb743c11e82008ccb6eb5201dc528aca3
k21971 <keith.simpson1971@gmail.com>
2021-08-01 02:33:58 +0000

    Update scripts/hints files for new editlevel version (38).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -22,9 +22,9 @@ COMPRESSBIN="/bin/gzip"
 NH_GIT="/home/build/NetHack37"
 NH_BRANCH="3.7-hdf" # will probably need to make another hdf branch for this one
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
-NHSUBDIR="nh370.37-hdf"
+NHSUBDIR="nh370.38-hdf"
 # VAR_PLAYGROUND from include/unixconf.h
-NH_VAR_PLAYGROUND="/nh370.37-hdf/var/"
+NH_VAR_PLAYGROUND="/nh370.38-hdf/var/"
 # only define this if dgl was configured with --enable-sqlite
 SQLITE_DBFILE="/dgldir/dgamelaunch.db"
 # END OF CONFIG
@@ -51,8 +51,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-nh370.37-hdf"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-nh370.37-hdf"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-nh370.38-hdf"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-nh370.38-hdf"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-nh370"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-nh370"
 
